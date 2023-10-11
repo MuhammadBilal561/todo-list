293f3a6a256c7a1ca6f4cf4907d292be4992da2e
MuhammadBilal561 <bilalrehan2006@gmail.com>
2023-10-11 11:39:46 -0700

    first commit

---
+++ dist/todolist.js
@@ -0,0 +1,37 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todo = [];
+let loop = true;
+while (loop) {
+    const answers = await inquirer.prompt([
+        {
+            "type": "string",
+            "name": "Todo",
+            "message": "What do you want to add in your Todo? "
+        },
+        {
+            "type": "confirm",
+            "name": "addmore",
+            "message": " Do you want to add more?",
+            "default": false
+        }
+    ]);
+    const { Todo, addmore } = answers;
+    console.log(answers);
+    loop = addmore;
+    if (Todo) {
+        todo.push(Todo);
+    }
+    else {
+        console.log("kindly add valid input");
+    }
+}
+if (todo.length > 0) {
+    console.log("your todo list");
+    todo.forEach(todo => {
+        console.log(todo);
+    });
+}
+else {
+    console.log("NO Todos found");
+}
